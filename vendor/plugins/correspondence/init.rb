ff1e5cc473b4248c24a9729f07e59f85bcef9e3f
Willem van Kerkhof <wvk@consolving.de>
2013-01-12 18:16:52 +0100

    Remove obsolete plugins

--- vendor/plugins/correspondence/init.rb
+++
@@ -1,6 +0,0 @@
-# ActionController::Routing::Routes.draw do |map|
-#   map.resources :letters
-# end
-#
-# ::NAVIGATION_ENTRIES << ['Korrespondenz', :letters_path]
-#
--- vendor/plugins/correspondence/tasks/correspondence_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :correspondence do
-#   # Task goes here
-# end
