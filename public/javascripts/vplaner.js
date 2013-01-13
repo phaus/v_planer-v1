0d6d7dc1a44ca9c1a9b913451299b3d93bda9a49
Philipp Haußleiter <philipp@haussleiter.de>
2013-01-13 21:25:38 +0100

    next updates for bootstrap

---
+++ public/javascripts/vplaner.js
@@ -0,0 +1,13 @@
+/**
+ * vplaner.js
+ * 13.01.2013
+ * @author Philipp Haussleiter
+ *
+ */
+jQuery(function($) {
+    // this is a fix for clearing non-JS Browsers captions.
+    function setup(){
+        $('.dropdown-toggle').dropdown();
+    }
+    setup();
+});
